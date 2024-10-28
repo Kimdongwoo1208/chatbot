323255b0859270843a929b0f1da9e1761c358989
Kimdongwoo1208 <ehddn090912@gmail.com>
2024-10-28 15:14:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,20 +2,33 @@ from dotenv import load_dotenv
 import streamlit as st
 import os
 import google.generativeai as genai
-
-load_dotenv()   ## load all environment variables
-
+from PIL import Image
+ 
+load_dotenv()
+ 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-st.set_page_config(page_title="My Chat Bot",page_icon="🗣️")
-
-st.header("My Chat Bot Web Application")
-
-question = st.text_input("Write a question here....")
-
+ 
+st.set_page_config(page_title="Image Chat Bot",page_icon="🗣️")
+ 
+st.header("My Image Chat Bot Web Application")
+ 
+question = st.text_input("Write your question here...")
+ 
+uploaded_image = st.file_uploader("Choose an Image..",type=["jpg","png","jpeg"])
+ 
+image = ""
+ 
 submit = st.button("Submit")
-
+ 
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    st.image(image,caption="Uploaded Image",use_column_width=True)
+ 
 if submit:
     model = genai.GenerativeModel("gemini-1.5-flash")
-    answer = model.generate_content(question)
-    st.write(answer.text)
+    if(input!=""):
+        response = model.generate_content([question,image])
+    else:
+        response = model.generate_content(image)
+ 
+    st.write(response.text)
